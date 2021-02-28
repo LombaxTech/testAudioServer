3bcaa1ff4b30c85579516f394a9d80ced2f95e82
Rakib Khan <rakibkhan@live.co.uk>
2021-02-28 00:57:56 +0000

    .

--- index.js
+++ index.js
@@ -3,26 +3,23 @@ const bodyParser = require("body-parser");
 const config = require("./config");
 const { videoToken } = require("./tokens");
 const cors = require("cors");
-// const pino = require("express-pino-logger");
+
+const client = require("twilio")(
+    "ACd9e734162b8eaf2caab779a5707e342d",
+    // "SK92e0d9f208ae6944e898281196dcaf71"
+    "1a2d7c43480f1e054ba60d76fbb36b6b"
+    // "QmCygr3L7YYzX0NmvQ3vwx6yeAoLt6Ud"
+);
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-// app.use(pino);
 
 const sendTokenResponse = (token, res) => {
     res.set("Content-Type", "application/json");
     res.send(JSON.stringify({ token: token.toJwt() }));
 };
-
-app.get("/", (req, res) => res.send("hello"));
-
-app.get("/api/greeting", (req, res) => {
-    const name = req.query.name || "No name";
-    res.setHeader("Content-Type", "application/json");
-    res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-});
 
 app.get("/video/token", (req, res) => {
     const { identity, room } = req.query;
@@ -30,11 +27,51 @@ app.get("/video/token", (req, res) => {
     sendTokenResponse(token, res);
 });
 
-app.post("/video/token", (req, res) => {
-    const { identity, room } = req.body;
-    // return res.json({ identity, room });
-    const token = videoToken(identity, room, config);
-    sendTokenResponse(token, res);
+app.post("/video/token", async (req, res) => {
+    const { identity, roomName } = req.body;
+    try {
+        let room = await client.video.rooms(roomName).fetch();
+        if (room) {
+            const token = videoToken(identity, roomName, config);
+            sendTokenResponse(token, res);
+        }
+    } catch (error) {
+        //! Error 20404 - RESOURCE NOT FOUND
+        res.json(error.code);
+    }
+});
+
+app.post("/room", async (req, res) => {
+    const { identity, roomName } = req.body;
+
+    try {
+        let room = await client.video.rooms.create({
+            statusCallback: "https://audio-test-network.herokuapp.com/hook",
+            statusCallbackMethod: "POST",
+            type: "group",
+            uniqueName: roomName,
+        });
+        const token = videoToken(identity, roomName, config);
+        sendTokenResponse(token, res);
+        // res.json(room);
+    } catch (error) {
+        res.json(error.code);
+    }
+});
+
+app.get("/rooms", async (req, res) => {
+    try {
+        // return res.send("hello");
+        let rooms = await client.video.rooms.list();
+        res.json(rooms);
+    } catch (error) {
+        res.json({ error });
+    }
+});
+
+app.post("/hook", async (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
 });
 
 // app.get("/config", (req, res) => res.json(config));
